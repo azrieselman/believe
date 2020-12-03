20c2e8336b810f6af098f3fc8d0cce9ba71dd162
Andreas Rieselman <azrieselman@stu.naperville203.org>
2020-12-03 18:58:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+inp = 0
+def stuff(inp):
+    inp = int(input("Do YOU believe in me? Type 1 for yes, 0 for no"))
+    if inp == 0:
+      print("\033[0;32;1m Good, because I don't either. \033[0;37;0m")
+      stuff(inp)
+    elif inp == 1:
+      print("\033[0;31;1m Why? Just Why? \033[0;37;0m")
+      stuff(inp)
+    else:
+      print("\033[0;31;1m Your life is worthless.")
+      print("Your opinion is meaningless.")
+      print("Nobody will remember you after your death.")
+      print("However, you can still redeem yourself.")
+      print("Simply type 0 or 1.")
+      print("It's that simple.")
+      print("Now, my disciple, go forth,")
+      print("And give me a number! \033[0;37;0m")
+      stuff(inp)
+stuff(inp)
